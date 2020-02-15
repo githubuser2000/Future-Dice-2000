dca9f7d2d74773f6224bab1d414968b0a0ca6a05
Alexander Kern <alexgit@alexkern.de>
2020-02-15 20:42:58 +0100

    Sa 15. Feb 20:42:58 CET 2020

--- dice.py
+++ dice.py
@@ -25,6 +25,7 @@ def poly(x,n,xe,e):
 def expo(x,n,xe,e):
     return pow(n,x) / pow(n,xe) * e
 
+
 def kombi(x,n,xe,e):
     randfktvar = random.randrange(6)+1
     randfktvar2 = random.randrange(6)+1
@@ -42,22 +43,9 @@ def kombi(x,n,xe,e):
         print("Kombi Root "+str(randfkt2[randfktvar])+" "+str(randfkt2[randfktvar2]))
         return pow(randfkt[randfktvar](x,n,xe,e), 1 / ( randfkt[randfktvar2](x,n,xe,e) + 1 ))
 
-randfkt = { 1 : lin,
-        2 : log,
-        3 : root,
-        4 : poly,
-        5 : expo,
-        6 : kombi }
-
-randfkt2 = { 1 : 'lin',
-        2 : 'log',
-        3 : 'root',
-        4 : 'poly',
-        5 : 'exp',
-        6 : 'kombi' }
-
-randfktvar = random.randrange(6)+1
-randfktvar2 = random.randrange(6)+1
+def gewicht(type1,x,n,xe,e,type2,n2,xe2,e2):
+    return ( fkt[type2](x,n,xe,e),
+            fkt[type1](x,n2,xe2,e2) )
 
 
 def rand(x,n,xe,e):
@@ -66,26 +54,86 @@ def rand(x,n,xe,e):
         print(str(randfkt2[randfktvar]))
     return randfkt[randfktvar](x,n,xe,e)
 
-
-
 fkt = { 'lin' : lin,
         'log' : log,
         'root' : root,
         'poly' : poly,
         'exp' : expo,
         'rand' : rand,
-        'kombi' : kombi}
+        'kombi' : kombi,
+        'gewicht' : gewicht}
+
+
+randfkt = { 1 : lin,
+        2 : log,
+        3 : root,
+        4 : poly,
+        5 : expo,
+        6 : kombi,
+        7 : gewicht}
+
+randfkt2 = { 1 : 'lin',
+        2 : 'log',
+        3 : 'root',
+        4 : 'poly',
+        5 : 'exp',
+        6 : 'kombi',
+        7 : 'gewicht' }
+
+randfktvar = random.randrange(6)+1
+randfktvar2 = random.randrange(6)+1
+
+def weightedrand(weights):
+    summ = 0
+    sum2 = []
+    for weight in weights:
+        summ += weight
+        sum2.append(summ)
+
+    rand1 = random.random() * summ
+
+    for i,asum in enumerate(sum2):
+        if rand1 < asum:
+            return i # +1
+
+    return None
+
+
+
+
+
 
 def main(inp):
-    until = inp[1]
-    inp[4] = int(inp[4])
-    inp[5] = float(inp[5])
-    inp[1] = int(inp[1])
-    if inp[4] <= inp[1] and inp[4] > 1:
-        for a in range(1,int(inp[1])+1):
-            print(str(a)+": "+str(fkt[inp[2]](int(a),int(inp[3]),int(inp[4]),inp[5])))
-        dice = random.randrange(inp[1])+1
-        print("Würfelwurf: "+str(fkt[inp[2]](dice,int(inp[3]),int(inp[4]),inp[5]))+" (Würfelaugen "+str(dice)+")")
+    if len(sys.argv) > 5 and len(sys.argv) < 7:
+        until = int(inp[1])
+        inp[4] = int(inp[4])
+        inp[5] = float(inp[5])
+        inp[1] = int(inp[1])
+        if inp[4] <= inp[1] and inp[4] > 1 and inp[2] != "gewicht":
+            for a in range(1,int(inp[1])+1):
+                print(str(a)+": "+str(fkt[inp[2]](int(a),int(inp[3]),int(inp[4]),inp[5])))
+            dice = random.randrange(inp[1])+1
+            print("Würfelwurf: "+str(fkt[inp[2]](dice,int(inp[3]),int(inp[4]),inp[5]))+" (Würfelaugen "+str(dice)+")")
+    elif len(inp) > 10 and inp[2] == "gewicht":
+        until = int(inp[1])
+        inp[5] = int(inp[5])
+        inp[6] = float(inp[6])
+        inp[1] = int(inp[1])
+        inp[8] = int(inp[8])
+        inp[9] = int(inp[9])
+        inp[10] = int(inp[10])
+        if inp[5] <= inp[1] and inp[5] > 1 and (len(inp) > 10 and inp[2] == "gewicht" ):
+            randos = []
+            values = []
+            for a in range(1,int(inp[1])+1):
+                thing = fkt[inp[2]](inp[3],int(a),int(inp[4]),int(inp[5]),inp[6],inp[7],int(inp[8]),int(inp[9]),inp[10])
+                randos.append(thing[0])
+                values.append(thing[1])
+                print(str(a)+": "+str(thing))
+            zeroTo_n_rand = weightedrand(randos)
+            print("rand augenzahl ergebnis: "+str(weightedrand(randos)))
+            #dice = random.randrange(inp[1])+1
+            print("Würfelwurf: "+str(values[zeroTo_n_rand])+" (Würfelaugen "+str(zeroTo_n_rand)+")")
 
 
 if len(sys.argv) > 5:
