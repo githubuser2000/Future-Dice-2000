d146c303b08f73be327d37d8fdf9c42e3a143cb3
Alexander Kern <alexgit@alexkern.de>
2020-05-24 09:49:27 +0200

    So 24. Mai 09:49:27 CEST 2020

--- government.py
+++ government.py
@@ -15,6 +15,8 @@ import random
 systemTypeMaps =  {'strnum' : {'democracy': 0, 'plutocracy' : 1, 'dictatorship' : 2, 'tyrannis': 3, 'aristocracy' : 4, 'oligarchy' : 5},'numstr':{0:'democracy',1:'plutocracy',2:'dictatorship',3:'tyrannis',4:'aristocracy',5:'oligarchy'}}
 systemTypes = systemTypeMaps['numstr'].values()
 
+argv = sys.argv
+
 def writeCsv(data):
     print(str(data))
     with open(data[1]+'.txt', mode='a+') as csv_file:
@@ -63,7 +65,7 @@ def orderOfPrecedence(argv, differentOrder):
 
 
 #def whoIsNotVotedAnymore(argv, whoNot):
-#    allLastVotesAndGovernment = readCsv(sys.argv)
+#    allLastVotesAndGovernment = readCsv(argv)
 #    government = allLastVotesAndGovernment[-1][1:]
 #    names = []
 #    for oneNot in whoNot:
@@ -76,7 +78,7 @@ whoHasMax = set()
 def peopleAlreadyDemocraticOrRandomlySelectedInPast(ObjDice=None):
     global whoHasMax
     print("peopleAlreadyDemocraticOrRandomlySelectedInPast")
-    for choice in readCsv(sys.argv)[:-1]:
+    for choice in readCsv(argv)[:-1]:
         if choice[0] == 'next':
             ObjDice.wuerfelAugenSet.add(len(choice)-2)
             print("last "+ str(len(choice)-2))
@@ -95,6 +97,14 @@ def peopleAlreadyDemocraticOrRandomlySelectedInPast(ObjDice=None):
                     print("last "+ str(i))
     return whoHasMax
 
+def twistGewichtung(argv):
+    arev = argv[3:]
+    arev.reverse()
+    for i,rev in enumerate(arev):
+        if i % 3 == 0:
+            print(str(rev))
+            argv[i+5] = rev
+    return argv
 
 
 
@@ -108,14 +118,14 @@ def newSystem(auswahl, argv, oldsystem=systemTypeMaps['numstr'][3]):
             if type(someone) is tuple:
                 people.append(someone[3])
     else:
-        people = readCsv(sys.argv)[-1][1:]
+        people = readCsv(argv)[-1][1:]
 
-    print(str(sys.argv[0:3]+people))
-    writeCsv(sys.argv[0:3]+people)
+    print(str(argv[0:3]+people))
+    writeCsv(argv[0:3]+people)
 
 def voting(userAmount, votes, aristrokratsAreLessThanAll=False, potentials=None, voteHierarchy=0, oligarchy=False):
     global whoHasMax
-    aristokratenAmount = math.floor(math.sqrt(len(sys.argv[3:])/3+2))
+    aristokratenAmount = math.floor(math.sqrt(len(argv[3:])/3+2))
     results = {}
     if userAmount == len(whoHasMax):
         revolution()
@@ -134,8 +144,8 @@ def voting(userAmount, votes, aristrokratsAreLessThanAll=False, potentials=None,
                     ( voteHierarchy > 0 and voteHierarchy - 1 <= i) or \
                     ( voteHierarchy < 0 and -voteHierarchy - 1 >= i):
                     if not i in whoHasMax:
-                        #if oligarchy and i == k:
-                        #    potential = int(oligarchy) * 3
+                        if oligarchy and i == k:
+                            potential = int(oligarchy) * 3
                         if not i in results.keys():
                             results[i] = 1 * int(potential)
                         else:
@@ -172,7 +182,7 @@ def voting2(argv, aristrokratsAreLessThanAll=False,Plutocracy=False,voteHierarch
 
 def hierarchy(argv, auswahl):
         print("next in hierarchy")
-        systempeople = readCsv(sys.argv)[-1][1:] # Menschen in ihrer Reihenfolge, wie sie vom System anfangs festgelegt wurden
+        systempeople = readCsv(argv)[-1][1:] # Menschen in ihrer Reihenfolge, wie sie vom System anfangs festgelegt wurden
         # in argv stehen die aktuelleren people drin und anders drin, d.h. mit 2
         # zahlen in arrayelementen jeweils
         longvar = ("dice.py "+str(auswahl)+" gewicht lin 1 1 1 lin 1 1 1").split()
@@ -214,7 +224,7 @@ def revolution():
     # von modulo 2 = 1 auf irgendein anderes x modulo 2 = 0
     # aber nicht das, was das entsprechende x modulo 2 = 0 wäre das davor liegen
     # würde
-    oldsystem = readCsv(sys.argv)[-1][0]
+    oldsystem = readCsv(argv)[-1][0]
     print('oldsystem: '+str(systemTypeMaps['strnum'][oldsystem]))
     if systemTypeMaps['strnum'][oldsystem] % 2 == 0:
         print('mod == 0')
@@ -224,29 +234,29 @@ def revolution():
         randbool = random.randint(0, 1) # randint 0 1 macht 0 oder 1 möglich
         newsystem = systemTypeMaps['numstr'][(systemTypeMaps['strnum'][oldsystem]+1+(2*randbool))%6]
     print(newsystem)
-    argv = sys.argv
+    argv = argv
     argv[2] = newsystem
     newSystem(auswahl, argv, oldsystem)
 
 #def __init__(self,inp,werfen = 2, uniq_ = False, bezeichner : str = "", negativ = False, median = False):
 if True:
-    app =QApplication(sys.argv)
+    app =QApplication(argv)
     #libdice.dice.languages1(app)
     qAppEngin = QQmlApplicationEngine()
     libdice_strlist = [qAppEngin.tr('lin'), qAppEngin.tr('log'), qAppEngin.tr('root'), qAppEngin.tr('poly'), qAppEngin.tr('exp'), qAppEngin.tr('kombi'), qAppEngin.tr('logistic'), qAppEngin.tr('rand'), qAppEngin.tr('gewicht'), qAppEngin.tr('add'), qAppEngin.tr('mul'), qAppEngin.tr("Wuerfelwurf: "),qAppEngin.tr(" (Wuerfelaugen ")]
     blub = [qAppEngin.tr('test')]
     libdice.dice.languages2(libdice_strlist)
 
-auswahl=int((len(sys.argv)-3)/3)
+auswahl=int((len(argv)-3)/3)
 
-if sys.argv[2] in systemTypes:
-#    print(str(sys.argv[3:]))
-    newSystem(auswahl, sys.argv)
-elif sys.argv[2] in ['revolution']:
+if argv[2] in systemTypes:
+#    print(str(argv[3:]))
+    newSystem(auswahl, argv)
+elif argv[2] in ['revolution']:
     revolution()
-elif sys.argv[2] in ['vote']:
-    historyThisGovernment = readCsv(sys.argv)
-    argv = sys.argv
+elif argv[2] in ['vote']:
+    historyThisGovernment = readCsv(argv)
+    argv = argv
     argv= argv[:3] + orderOfPrecedence(argv,historyThisGovernment[-1][1:])
     print(str(argv))
     whoHasMax = peopleAlreadyDemocraticOrRandomlySelectedInPast()
@@ -265,7 +275,7 @@ elif sys.argv[2] in ['vote']:
     elif historyThisGovernment[-1][0] == systemTypeMaps['numstr'][4]: # Aristrokratie
         print("vote in Aristokratie")
         #longvar = ("dice.py "+str(auswahl)+" gewicht lin 1 1 1 lin 1 1 1").split()
-        #hierarchyGame = libdice.dice(longvar, werfen=0, uniq_=True, bezeichner=' '.join(sys.argv[4:]))
+        #hierarchyGame = libdice.dice(longvar, werfen=0, uniq_=True, bezeichner=' '.join(argv[4:]))
         #print('out: '+str(hierarchyGame.out()))
         value = voting2(argv, True)
     elif historyThisGovernment[-1][0] == systemTypeMaps['numstr'][1]: # Plutokratie
@@ -276,8 +286,8 @@ elif sys.argv[2] in ['vote']:
 #    if value[2] == 'vote':
  #       writeCsv(value)
 #
-#elif sys.argv[2] in ['next']:  # Tyranei und Dictatorship: beides Hierarchie, aber jeder der dran ist hat die Wahl sich oder höher bei Tyranei oder sich oder niedriger bei Dictatorship
-#    historyThisGovernment = readCsv(sys.argv)
+#elif argv[2] in ['next']:  # Tyranei und Dictatorship: beides Hierarchie, aber jeder der dran ist hat die Wahl sich oder höher bei Tyranei oder sich oder niedriger bei Dictatorship
+#    historyThisGovernment = readCsv(argv)
 #    print(historyThisGovernment[-1][0])
     elif historyThisGovernment[-1][0] == systemTypeMaps['numstr'][2]: # Dictatorship
         value = hierarchy(argv, auswahl)
